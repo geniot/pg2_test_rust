ac98374e1c6e74a2a39fe08394955797cfcacb89
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2023-01-17 18:15:20 +0100

    Save window state

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,9 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let window = video_subsystem
         .window("PG2 Hardware Test", settings.get(SettingKey::Width) as u32, settings.get(SettingKey::Height) as u32)
-        .position(settings.get(SettingKey::XPos), settings.get(SettingKey::YPos))
+        .position(settings.get(SettingKey::XPos) as i32, settings.get(SettingKey::YPos) as i32)
         .resizable()
+        .set_window_flags(settings.get(SettingKey::Flags) as u32)
         .build()
         .map_err(|e| e.to_string())?;
 
@@ -51,12 +52,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         // The rest of the game loop goes here...
     }
 
-    settings.set(SettingKey::XPos, canvas.window().position().0);
-    settings.set(SettingKey::YPos, canvas.window().position().1);
-    settings.set(SettingKey::Width, canvas.window().size().0 as i32);
-    settings.set(SettingKey::Height, canvas.window().size().1 as i32);
-
-    settings.save();
+    settings.save(canvas.window());
 
     // prefs.insert("xpos".into(), canvas.window().position().0.to_string());
     // prefs.insert("ypos".into(), canvas.window().position().1.to_string());
--- src/settings.rs
+++ src/settings.rs
@@ -4,6 +4,8 @@ use std::collections::HashMap;
 use abserde::*;
 use serde::{Serialize, Deserialize};
 use std::string::ToString;
+use sdl2::sys::SDL_WindowFlags;
+use sdl2::video::Window;
 use SettingKey::*;
 
 pub struct Settings {
@@ -22,6 +24,7 @@ pub enum SettingKey {
     YPos = 0x01,
     Width = 0x02,
     Height = 0x03,
+    Flags = 0x04,
 }
 
 impl Settings {
@@ -37,24 +40,35 @@ impl Settings {
             Err(..) => MyConfig { user_data: HashMap::new() },
         };
 
+        let default_window_flags: u32 = (SDL_WindowFlags::SDL_WINDOW_SHOWN as u32 | SDL_WindowFlags::SDL_WINDOW_RESIZABLE as u32) as u32;
+
         conf.user_data.entry(XPos.to_string()).or_insert_with(|| 100.to_string());
         conf.user_data.entry(YPos.to_string()).or_insert_with(|| 100.to_string());
         conf.user_data.entry(Width.to_string()).or_insert_with(|| 320.to_string());
         conf.user_data.entry(Height.to_string()).or_insert_with(|| 240.to_string());
+        conf.user_data.entry(Flags.to_string()).or_insert_with(|| default_window_flags.to_string());
 
         Settings { my_abserde, config: conf }
     }
 
-    pub fn get(&self, setting_key: SettingKey) -> i32 {
+    pub fn get(&self, setting_key: SettingKey) -> u32 {
         let val_str: Option<&String> = self.config.user_data.get(&*setting_key.to_string());
         val_str.unwrap().parse().unwrap()
     }
 
-    pub fn set(&mut self, setting_key: SettingKey, val: i32) {
+    pub fn set(&mut self, setting_key: SettingKey, val: u32) {
         self.config.user_data.insert(setting_key.to_string(), val.to_string());
     }
 
-    pub fn save(&self) {
+    pub fn save(& mut self, window: &Window) {
+        if window.window_flags() as u32 & SDL_WindowFlags::SDL_WINDOW_MAXIMIZED as u32 <= 0 {
+            self.set(XPos, window.position().0 as u32);
+            self.set(YPos, window.position().1 as u32);
+            self.set(Width, window.size().0);
+            self.set(Height, window.size().1);
+        }
+        self.set(Flags, window.window_flags());
+
         self.config.save_config(&self.my_abserde).unwrap();
     }
 }
